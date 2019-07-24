59dbec91fa81c2aedef2468cee64237bf1c80e9f
tanxingsong <tanxingsong@cetcbigdata.com>
2019-07-24 20:28:20 +0800

    feat : 新增Eureka微服务

---
+++ ms-govern-eureka/src/main/java/com/ttcode/govern/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.ttcode.govern.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : xingsongtan@qq.com
+ * @date : 20:19 2019/7/24
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
