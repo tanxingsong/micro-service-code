dc218ddf13f964828280680adbbbd0d392c48cee
tanxingsong <tanxingsong@cetcbigdata.com>
2019-07-28 17:31:02 +0800

    feat : 添加spring cloud config

---
+++ ms-govern-config/src/main/java/com/ttcode/govern/config/ConfigApplication.java
@@ -0,0 +1,19 @@
+package com.ttcode.govern.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author : xingsongtan@qq.com
+ * @date : 17:03 2019/7/28
+ */
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
